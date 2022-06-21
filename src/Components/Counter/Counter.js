b042595a04e1594270acf03241ccf80d0b9c3490
alejodev2103 <alejod2103@gmail.com>
2022-06-21 12:11:48 -0600

    ajuste de containers

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -4,7 +4,7 @@ import './Counter.css'
 function Counter() {
   return (
     <>
-    <div class="container-xxl py-5">
+    <div class="container-fluid py-5">
         <div class="containerFeature">
             <div class="row g-5">
                 <div class="featurecont  col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.1s">
--- src/Components/Servicios/Service.js
+++ src/Components/Servicios/Service.js
@@ -10,7 +10,7 @@ import { LlamaBoton } from './ServiceStyles.js'
 function Service() {
   return (
     <>
-     <div class="container-xxl py-5">
+     <div class="container-fluid py-5">
         <div class="container">
             <div class="text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style={{maxWidth: "600px"}}>
                 <h6 class="text-warning">Nuestros Servicios</h6>
